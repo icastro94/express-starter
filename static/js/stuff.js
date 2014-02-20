007d0cd0d71c03626093d5af019e7230fa4af149
Isaac Castro <socalnative94@yahoo.com>
2014-02-19 19:52:56 -0800

    Completed Houdini and Pink Problems!

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -8,17 +8,18 @@ $('#say_hello').click(function() {
 
 // Problem 2 (Houdini) ------------------------------------------------------
 $('#disappear').click(function() {
-  
+  $('#houdini_text').hide();
 });
 
 $('#reappear').click(function() {
-  //WRITE CODE HERE
+  $('#houdini_text').show();
 });
 
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
-// WRITE CODE HERE
-
+$('#turn_me_pink').click(function() {
+  $('#tickled_text').css('color','pink');
+});
 
 // Problem 4 (Greet Me) -----------------------------------------------------
-// WRITE CODE HERE
+
