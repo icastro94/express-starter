4d79934418c7e8f3b808380530499ed80d4fa649
Isaac Castro <socalnative94@yahoo.com>
2014-04-16 19:39:13 -0700

    Finished Ball Bounce

--- static/js/ball.js
+++ static/js/ball.js
@@ -6,20 +6,45 @@ $(document).ready(function() {
   var height = canvas.height;
 
   //PUT STUFF HERE
-  var x = 20
-
+  
+  var ball = {
+    x: 20,
+    y: 20,
+    r: 20,
+    vx: 2,
+    vy: 1,
+  };
 //run an iteration of the game
   var updateGame = function() {
-      context.fillStyle = "black"
+      context.fillStyle = "black";
       context.fillRect(0, 0, width, height);
-      
-      context.fillStyle = "red"
+
+      context.fillStyle = "red";
       context.beginPath()
-       context.arc(x, 20, 20, Math.PI, -Math.PI);
+       context.arc(ball.x, ball.y, ball.r, Math.PI, -Math.PI);
       context.closePath()
       context.fill();
-    x = x + 5
+    
+    if (ball.x + .5*ball.r >= width) {
+      ball.vx = -ball.vx
+    } else {};
+
+    if (ball.x - .5*ball.r <= 0) {
+      ball.vx = -ball.vx
+    } else {};
+
+    if (ball.y + .5*ball.r >= height) {
+      ball.vy = -ball.vy
+    } else {};
+
+    if (ball.y - .5*ball.r <= 0) {
+      ball.vy = -ball.vy
+    } else {};
+   
+    ball.x = ball.x + ball.vx;
+    ball.y = ball.y + ball.vy;
   setTimeout(updateGame, 10);
-    };
+  
+  };
   updateGame();
 });
